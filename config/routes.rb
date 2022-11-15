8423c7867083c3c43660bba1d6617d6dde2f3dbd
nicolas naas <nicolas_naas@outlook.fr>
2022-11-15 14:26:21 +0100

    Task Manager without CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
- 
+  get "tasks", to: "tasks#index"  #Indiquer la route des tasks
+  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
